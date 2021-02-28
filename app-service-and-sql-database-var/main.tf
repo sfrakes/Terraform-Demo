30f2058ee077da29fa288747a8e838fcb725fab5
StuartFrakes <stuart.frakes@recordsure.com>
2021-02-28 19:21:20 +0000

    Update main.tf

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -8,11 +8,3 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
-
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["10.1.0.0/16"]
-}
