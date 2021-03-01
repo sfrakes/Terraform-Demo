d8bd7c9519db8c1e4dec4a18a8c81ec2de47a34d
StuartFrakes <stuart.frakes@recordsure.com>
2021-03-01 19:10:32 +0000

    Update main.tf

--- rg/main.tf
+++ rg/main.tf
@@ -46,9 +46,8 @@ resource "azurerm_windows_virtual_machine" "example" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_F2"
-  admin_username      = "adminuser"
-  admin_password      = "P@$$w0rd1234!"
-  availability_set_id = azurerm_availability_set.DemoAset.id
+  admin_username      = "rsadmin"
+  admin_password      = "C0lumbiana12"
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
