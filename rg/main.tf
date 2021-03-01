f76909967327c6b8e6af9eb5528d95cb4f8c94aa
StuartFrakes <stuart.frakes@recordsure.com>
2021-03-01 18:50:11 +0000

    Update main.tf

--- rg/main.tf
+++ rg/main.tf
@@ -1,9 +1,10 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
+  version = "=2.0.0"
   features {}
 }
 
 resource "azurerm_resource_group" "RG-Terraform" {
-  name     = "simple-resource-group"
+  name     = "MPN-resource-group"
   location = "uksouth"
 }
