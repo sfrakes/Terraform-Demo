87659ae7a0ba0b55ff8af0167e7cc3ef74d92d9c
StuartFrakes <stuart.frakes@recordsure.com>
2021-03-02 17:52:10 +0000

    Update variables.tf

--- 2019-vm/variables.tf
+++ 2019-vm/variables.tf
@@ -1,20 +1,20 @@
-variable “node_location” {
+variable "node_location" {
 type = string
 }
-variable “resource_prefix” {
+variable "resource_prefix" {
 type = string
 }
-variable “node_address_space” {
-default = [“10.250.0.0/16”]
+variable "node_address_space" {
+default = ["10.250.0.0/16"]
 }
 #variable for network range
-variable “node_address_prefix” {
-default = “10.250.1.0/24”
+variable "node_address_prefix" {
+default = "10.250.1.0/24"
 }
 #variable for Environment
-variable “Environment” {
+variable "Environment" {
 type = string
 }
-variable “node_count” {
+variable "node_count" {
 type = number
 }
