8498e1eee280eba1541b4a0d251148ae93de3bb6
StuartFrakes <stuart.frakes@recordsure.com>
2021-03-02 17:09:36 +0000

    commit

--- 2019-vm/variables.tf
+++ 2019-vm/variables.tf
@@ -1,5 +1,20 @@
-variable "arm_endpoint" {}
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable “node_location” {
+type = string
+}
+variable “resource_prefix” {
+type = string
+}
+variable “node_address_space” {
+default = [“10.250.0.0/16”]
+}
+#variable for network range
+variable “node_address_prefix” {
+default = “10.250.1.0/24”
+}
+#variable for Environment
+variable “Environment” {
+type = string
+}
+variable “node_count” {
+type = number
+}
