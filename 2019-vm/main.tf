60819c23daf79c8582dd3d9fdacef5929b7001c9
StuartFrakes <stuart.frakes@recordsure.com>
2021-03-04 15:59:30 +0000

    Update main.tf

--- 2019-vm/main.tf
+++ 2019-vm/main.tf
@@ -90,7 +90,7 @@ version = "latest"
 
 os_disk {
 #name = "myosdisk-${count.index + 1}"
-name = "${var.resource_prefix}${format("%02d", count.index + 1)}"
+name = "${var.resource_prefix}_osdisk${format("%02d", count.index + 1)}"
 caching = "ReadWrite"
 storage_account_type = "Standard_LRS"
 }
